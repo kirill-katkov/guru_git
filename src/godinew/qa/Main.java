51f2d17ba7c546ca795be056f55d99e2eacd2dee
Kirill Katkov <k.katkov@dodopizza.com>
2022-03-30 15:05:05 +0300

    Update Main.java

--- src/godinew/qa/Main.java
+++ src/godinew/qa/Main.java
@@ -6,4 +6,8 @@ public class Main {
         System.out.println("Hello, QA guru!");
         System.out.println("Another Line");
     }
+    
+    public static void another() {
+        System.out.println("Another method");
+    }
 }
